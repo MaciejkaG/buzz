2e5db60a23b18729ced9833209105868645eb9fe
Maciej Gomola <me@mcjk.cc>
2025-01-20 16:10:44 +0100

    Unfinished landing bg animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import '@/components/home.css';
+
 export default function Home() {
   return (
     <div>
       <div className="block overflow-scroll w-full h-screen">
-        <div className="w-96 h-96 bg-red-500 blur-[15rem]">
-
-        </div>
+        <div className="w-96 h-96 bg-red-500 blur-[15rem] absolute animation-1"></div>
+        <div className="w-96 h-96 bg-cyan-500 blur-[15rem] absolute animation-2"></div>
       </div>
     </div>
   );
